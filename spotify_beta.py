0b7c2d1e5acea512d769249574a11fd9974cf810
ProfessorFjord4 <professorfjord4@gmail.com>
2023-04-21 00:22:56 +0900

    コメントを追加しました。

--- spotify_beta.py
+++ spotify_beta.py
@@ -29,8 +29,8 @@ my_secret = api_datalist[4] #client secret
 ccm = SpotifyClientCredentials(client_id = my_id, client_secret = my_secret)
 spotify = spotipy.Spotify(client_credentials_manager = ccm, language='ja')
 
-user = api_datalist[5]
-playlist_id = api_datalist[6]
+user = api_datalist[5] #Spotifyのusername
+playlist_id = api_datalist[6] #対象とするSpotifyプレイリストのid
 
 #日付
 today = datetime.date.today()
@@ -38,6 +38,8 @@ month = str(today.month)
 day = str(today.day)
 
 # db接続
+# dbは以下の文で定義されたテーブル1つで構成されています
+# CREATE TABLE music(id integer, track_name text, artist text, date text);
 dbname = 'mastodon.db'
 conn = sqlite3.connect(dbname)
 cur = conn.cursor()
@@ -52,21 +54,24 @@ list = playlist_data['tracks']['items']
 cur.execute("select count(*) from music")
 db_count = cur.fetchone()[0] - 1
 
+# 新しい曲が追加されたとき
 if len(list) > db_count:
     # dbから最新の曲のidを取得
     cur.execute("select id, track_name from music order by id desc limit 1")
     db_latest_song = cur.fetchone()
 
+    # 現在投稿されているリストの最も下の曲を取得
     for i in reversed(range(len(list))):
       if(list[i]['track']['name'] == db_latest_song[1]):
         latest_track = list[i+1]['track']
         break
       
-    #曲のデータ
+    #投稿する曲のデータ
     track_name = latest_track['name']
     artist_name = latest_track['album']['artists'][0]['name']
     track_url = latest_track['external_urls']['spotify']
     
+    # トゥート文
     toot_text = month + '/' + day + '\n' + track_name + ' - ' + artist_name + '\n' + track_url
 
     # リプライの形式で追加
@@ -77,6 +82,7 @@ if len(list) > db_count:
     user_dict = api.account_verify_credentials()
     user_toots = api.account_statuses(user_dict['id'], limit=1)
 
+    # リプライの形式で追加するためにDBに保存
     insert_text = "INSERT INTO music VALUES(" + str(user_toots[0]['id']) + ", '" + track_name + "', '" + artist_name + "', '" + str(today) + "')"
     cur.execute(insert_text)
     conn.commit()
